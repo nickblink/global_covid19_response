4c765623d04ea71cd6d6a7f44832384cb67171ae
Thinkpad E15 <nicklink415@gmail.com>
2024-07-17 16:00:21 -0400

    working on morans I - still need it robust to facs with no neighbs

--- R/morans_I_test.R
+++ R/morans_I_test.R
@@ -17,22 +17,54 @@ if(file.exists('C:/Users/Admin-Dell')){
 
 setwd(res_dir)
 
-load('WF_Poisson_NB_comparison_WFDGP_R100_04072024/sim_results_1(1).RData')
+get_morans_I <- function(df, out_col = 'y', start_date = NULL, end_date = NULL, avg_across_time = T){
+  # get the dates
+  dates <- unique(df$date)
+  if(!is.null(start_date)){dates <- dates[dates >= start_date]}
+  if(!is.null(end_date)){dates <- dates[dates <= end_date]}
 
-tt <- imputed_list[[1]]$df_miss
+  # get data adjacency matrix
+  #W <- make_district_adjacency(df)
+  
+  I_vec<- sapply(dates, function(dd){
+    # filter by date and non-missingness
+    tmp <- df %>% filter(date == dd)
+    tmp <- tmp[!is.na(tmp[,out_col]),]
+    
+    # get adjacency
+    W2 <- make_district_adjacency(tmp)
+    print(dd)
+    
+    browser()
+    # compute Moran's I.
+    y_exp = mean(tmp[,out_col])
+    tmp$residual <- tmp[,out_col] - y_exp
+    numerator <- sum(tmp$residual * W2%*%tmp$residual)/sum(W2)
+    denominator <- mean((tmp[,out_col] - y_exp)^2)
+    I_tmp <- numerator/denominator
+    return(I_tmp)
+  })
+  
+  if(avg_across_time){
+    return(mean(I_vec))
+  }else{
+    return(I_vec)
+  }
+}
 
-# Finding covariance within each time point.
-N = length(unique(tt$facility))
-Ttime = length(unqiue(tt$date))
+load('mcar01_car0303025_beta_06_beta1n025_negbin_2024_07_11/sim_results_p0.1_mcar_1(50).RData')
 
-tmp <- tt %>% filter(date == '2016-01-01')
-var_y = var
-y_exp = mean(tmp$y)
-mean((tmp$y - y_exp)^2)
+df <- imputed_list[[1]]$df_miss %>%
+  select(date, facility, district, year, month, y, y_true)
 
-TEST
-can use ape::moran.i to input adjacecny and data for each time point.
+get_morans_I(df)
+get_morans_I(df, out_col= 'y_true')
 
-# I'm confused. honestly I'm hungry. But if I do this and average across all time points that could add variance because of the high-missing time points.
-# If I compute with all points at once, then wouldn't that show a high I even if there's no spatial correlation? Like because of equal time points having similar trends. Yes. But the equal time points won't be used more than unequal time points. Boy there's gotta be a better way to say this.
 
+load('mcar05_car0303025_beta_06_beta1n025_negbin_2024_07_11/sim_results_p0.5_mcar_1(50).RData')
+
+df <- imputed_list[[1]]$df_miss %>%
+  select(date, facility, district, year, month, y, y_true)
+
+get_morans_I(df)
+get_morans_I(df, out_col= 'y_true')
